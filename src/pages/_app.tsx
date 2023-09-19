2513665690d448d2317562e920a996714b25ed2b
Rafael Manfrim <rafaelmanfrim2004@gmail.com>
2023-09-19 15:26:11 -0300

    Configurando reporter de acessibilidade com axe

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
+'use client'
+
 import '@/styles/globals.css'
+
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+
+import { axeAccessibilityReporter } from '@/utils/axeAccessibilityReporter'
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    axeAccessibilityReporter()
+  }, [])
+
   return <Component {...pageProps} />
 }
